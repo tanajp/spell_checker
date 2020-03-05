b6ce6993b31f5ac4d1a5aca45ccc420e8f770e9b
tanajp <50686226+tanajp@users.noreply.github.com>
2020-03-05 19:29:54 +0900

    言語モデル

--- blstm_lang_model.py
+++ blstm_lang_model.py
@@ -11,7 +11,7 @@ def wakati(text):
     w = t.tokenize(text, wakati=True)
     return " ".join(w)
 
-data = [wakati(w) for w in data]
+data = [wakati(w) for w in text]
 
 import numpy as np
 import pandas as pd
@@ -20,13 +20,13 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
 
-# モデルから文字列を生成.
+# モデルから文字列を生成
 def generate_seq(model, tokenizer, seed_text, n_words):
     in_text, result = seed_text, seed_text
     for _ in range(n_words):
         encoded = tokenizer.texts_to_sequences([in_text])[0]
         encoded = np.array(encoded)
-        # 学習データから文字列を予測.
+        # 学習データから文字列を予測
         yhat = model.predict_classes(encoded, verbose=0)
         out_word = ''
         for word, index in tokenizer.word_index.items():
@@ -41,7 +41,7 @@ tokenizer = Tokenizer()
 tokenizer.fit_on_texts([data])
 encoded = tokenizer.texts_to_sequences([data])[0]
 
-# 語彙のサイズを決定.
+# 語彙のサイズを決定
 vocab_size = len(tokenizer.word_index) + 1
 print('Vocabulary Size: %d' % vocab_size)
 
@@ -51,7 +51,7 @@ for i in range(1, len(encoded)):
     sequences.append(sequence)
 print('Total Sequences: %d' % len(sequences))
 
-# Xとy要素に分割する.
+# Xとy要素に分割する
 sequences = np.array(sequences)
 X, y = sequences[:,0],sequences[:,1]
 
@@ -68,10 +68,10 @@ print(model.summary())
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# 学習させる.
+# 学習させる
 model.fit(X, y, epochs=500, verbose=2)
 
-# 開始地点を決定.
+# 開始地点を決定
 typo_words = []
 typo_index = []
 if len(words) != len(sentence):
@@ -79,6 +79,13 @@ if len(words) != len(sentence):
         if not str(sentence[i]) in words:
             typo_words.append(str(sentence[i]))
             typo_index.append(sentence.index(typo_words[0]))
+            
+else:
+    for i in range(len(err)):
+        for j in range(len(sentence)):
+            if str(err[i]) == str(sentence[j]):
+                typo_words.append(str(err[i]))
+                typo_index.append(sentence.index(typo_words[0]))
 
 search_index = []
 search_word = []
